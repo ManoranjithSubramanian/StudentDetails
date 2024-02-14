e4a8035f8f378c90df95535d996640e10b17fa82
ManoranjithSubramanian <manoranjith711@gmail.com>
2024-02-15 01:25:56 +0530

    Created a second repository of the first

---
+++ demo/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,14 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	//Added By Mano
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+}
